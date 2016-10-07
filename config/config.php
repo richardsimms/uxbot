f0e889fd09c33d50ee7482c7d69bd8a3d421a42d
Richard Simms <simmsdesign@gmail.com>
2016-10-07 12:48:11 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -23,8 +23,8 @@ $siteURL="http://pacific-spire-28951.herokuapp.com/";
 
 
 // Slack Webhook integration
-$slackWebHookURL="https://hooks.slack.com/services/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";
-$slackRoom = "uxbot";
+$slackWebHookURL="https://hooks.slack.com/services/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";
+$slackRoom = "general";
 $slackBotName = "Stanley"; //THE NAME OF YOUR BOT
 $slackBotIcon = ":robot_face:"; // THE PROFILE IMAGE OF YOUR BOT example :money_mouth_face: or :shit:
 
